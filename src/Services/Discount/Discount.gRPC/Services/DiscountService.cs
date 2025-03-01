2fd18d87f94c3f5c2ffccafce3dd6d806fe3de7c
Ali Kağan Yılmaz <kaganylmz12@gmail.com>
2025-03-01 22:24:06 +0300

    [Discount] feat: docker containerization yml files

--- src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
+++ src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
@@ -1,29 +1,81 @@
 using Discount.Grpc;
 using Discount.gRPC.Data;
+using Discount.gRPC.Models;
 using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.gRPC.Services;
 
-public class DiscountService(DiscountContext context, ILogger<DiscountService> logger)
+public class DiscountService
     : DiscountProtoService.DiscountProtoServiceBase
+
 {
-    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    //mapster reinstalled in case grpc is not typical microservice
+    private DiscountContext _context;
+    private ILogger<DiscountService> _logger;
+
+    public DiscountService(DiscountContext context, ILogger<DiscountService> logger)
     {
-        return base.GetDiscount(request, context);
+        _logger = logger;
+        _context = context;
+    }
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await _context.Coupons.FirstOrDefaultAsync(x => string.Equals(x.ProductId,request.ProductId)) ??
+                     new Coupon()
+        {
+            ProductId = Guid.Parse(request.ProductId),
+            Amount = 0,
+            Description = "No Discount"
+        };
+
+        _logger.LogInformation("Discount is retrieved for ProductId: {ProductId}, Amount: {Amount}", coupon.ProductId, coupon.Amount);
+        return coupon.Adapt<CouponModel>();
     }
 
-    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
     {
-        return base.UpdateDiscount(request, context);
+        var coupon = await _context.Coupons.FirstOrDefaultAsync(x => string.Equals(x.ProductId,request.Coupon.ProductId));
+        if (coupon == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductId={request.Coupon.ProductId} is not found."));
+        }
+        coupon.Amount = request.Coupon.Amount;
+        coupon.Description = request.Coupon.Description;
+        await _context.SaveChangesAsync();
+        _logger.LogInformation("Discount is successfully updated. ProductId: {ProductId}, Amount: {Amount}", coupon.ProductId, coupon.Amount);
+        return coupon.Adapt<CouponModel>();
     }
 
-    public override Task<CouponModel> CreateDiscount(GetDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
     {
-        return base.CreateDiscount(request, context);
+        var coupon = new Coupon()
+        {
+            ProductId = Guid.Parse(request.Coupon.ProductId),
+            Amount = request.Coupon.Amount,
+            Description = request.Coupon.Description
+        };
+        
+        _context.Coupons.Add(coupon);
+        await _context.SaveChangesAsync();
+        _logger.LogInformation("Discount is successfully created. ProductId: {ProductId}, Amount: {Amount}", coupon.ProductId, coupon.Amount);
+        return coupon.Adapt<CouponModel>();
     }
 
-    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
     {
-        return base.DeleteDiscount(request, context);
+        var coupon = await _context.Coupons.FirstOrDefaultAsync(x=>string.Equals(x.ProductId,request.ProductId));
+        if (coupon is null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductId={request.ProductId} is not found."));
+        }
+        _context.Coupons.Remove(coupon);
+        await _context.SaveChangesAsync();
+        _logger.LogInformation("Discount is successfully deleted. ProductId: {ProductId}", request.ProductId);
+        return new DeleteDiscountResponse()
+        {
+            Success = true
+        };
     }
 }
